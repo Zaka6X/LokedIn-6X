5044b92697f85933f064a4044e388a0a3da08fef
Salim <salim.hamine@etu.uae.ac.ma>
2025-04-20 16:23:31 +0100

    updated code

---
+++ Node - MySql/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env' });
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+// Serve static files from the "public" directory
+const publicPath = path.join(__dirname, '../');
+app.use(express.static(publicPath));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.set('view engine', 'hbs');
+
+db.connect((e) => {
+    if (e) {
+        console.log(e);
+    } else {
+        console.log("Database Connected!");
+    }
+});
+
+// Serve index.html on the root route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+//Define routes
+const authRouter = require('./routes/auth');
+app.use('/auth', authRouter);
+
+app.listen(4000, () => {
+    console.log("Server Started on Port 4000");
+});
